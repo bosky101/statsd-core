758040d415db8190a27ada38451095aee55e8d55
Bosky <bosky@includetech.co>
2013-08-08 11:56:55 +0530

    added config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Place this where you docker build / docker pull
+ * For example: see https://github.com/etsy/statsd/blob/master/exampleConfig.js
+ * It must be named config.js
+
+ * My first docker image. Enjoy!
+ * bosky101
+ */
+{"graphitePort": 2003, "graphiteHost": "localhost", "port": 8125}
+
